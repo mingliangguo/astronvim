3eea658272df603e0d808d0221cb959e8581c2ef
Mingliang Guo <me@mingliang.me>
2024-10-27 14:48:33 -0400

    feat: update vimwiki config

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -20,6 +20,35 @@ return {
           ext = ".md",
         },
       }
+      
+      vim.g.vimwiki_syntax_plugins = {
+	      codeblock = {
+		      ["```lua"] = { parser = "lua" },
+		      ["```python"] = { parser = "python" },
+		      ["```javascript"] = { parser = "javascript" },
+		      ["```bash"] = { parser = "bash" },
+		      ["```html"] = { parser = "html" },
+		      ["```css"] = { parser = "css" },
+		      ["```c"] = { parser = "c" },
+	      },
+      }
+
+      vim.g.vimwiki_auto_header = 1
+      vim.g.vimwiki_ext2syntax = {
+        ['.md'] = 'markdown',
+        ['.markdown'] = 'markdown',
+        ['.mdown'] = 'markdown',
+      }
+      vim.g.vimwiki_global_ext = 0 -- don't treat all md files as vimwiki (0)
+      vim.g.vimwiki_folding = "list"
+      vim.g.vimwiki_hl_headers = 1  -- use alternating colours for different heading levels
+      vim.g.vimwiki_markdown_link_ext = 1 -- add markdown file extension when generating links
+      vim.g.taskwiki_markdown_syntax = "markdown"
+      vim.g.indentLine_conceallevel = 2 -- indentline controlls concel
+      --vim.set.o.conceallevel = 1 -- so that I can see `` and full urls in markdown files
+
+      vim.api.nvim_set_keymap("n", "<F3>", ":VimwikiDiaryPrevDay<CR>", { noremap = true, silent = true, nowait = true })
+      vim.api.nvim_set_keymap("n", "<F4>", ":VimwikiDiaryNextDay<CR>", { noremap = true, silent = true, nowait = true })
     end,
     config = function() end,
   },
