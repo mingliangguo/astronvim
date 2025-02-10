2b8e830c8bc42d79d87d4bf8e47b05a0eb871d30
Mingliang <689504+mingliangguo@users.noreply.github.com>
2025-02-10 11:44:33 -0500

    pre-format json files to avoid slowness

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -18,6 +18,12 @@ return {
         },
       },
       vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {}),
+      vim.keymap.set("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts),
+      -- preformat json with jq
+      vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
+        pattern = "*.json",
+        command = ":%!jq .",
+      }),
       buffers = { follow_current_file = { enabled = true } },
     },
   },
