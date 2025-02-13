fb4cd9a4a6a06a258aef92d499f17f705bb5e2e4
Mingliang <689504+mingliangguo@users.noreply.github.com>
2025-02-13 11:00:25 -0500

    chore: use catppuccin colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "shaunsingh/nord.nvim",
+  "catppuccin/nvim",
   priority = 1000,
   config = function()
     vim.opt.termguicolors = true
-    vim.cmd.colorscheme "nord"
+    vim.cmd.colorscheme "catppuccin"
   end,
 }
